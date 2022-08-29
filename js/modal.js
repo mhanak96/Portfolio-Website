260809a1d1c24aed61009a84e9badbce8df54f5f
mhanak96 <mateuszhanak@protonmail.com>
2022-08-29 22:14:26 +0200

    v7

--- js/modal.js
+++ js/modal.js
@@ -1,8 +1,32 @@
 'use strict'
-document.querySelectorAll('.cert-list img').forEach(image => {
-    image.onclick = () => {
-        document.querySelector('.popup-cerf').style.display = 'block';
-        document.querySelector('.popup-cerf img').src = image.getAttribute('src');
-    }
-    
+
+const harv = document.querySelector('.harv');
+const harvi = document.querySelector('.harv-img');
+
+const skp = document.querySelector('.response');
+const skpi = document.querySelector('.skp-img');
+
+const sgs = document.querySelector('.ui');
+const sgsi = document.querySelector('.ui-img');
+
+const exitBtn = document.querySelector('.close-btn');
+
+harv.addEventListener('click', function(){
+    document.querySelector('.popup-cerf').style.display = 'block';
+    document.querySelector('.popup-cerf img').src = harvi.getAttribute('src');
+});
+
+skp.addEventListener('click', function(){
+    document.querySelector('.popup-cerf').style.display = 'block';
+    document.querySelector('.popup-cerf img').src = skpi.getAttribute('src');
+});
+
+sgs.addEventListener('click', function(){
+    document.querySelector('.popup-cerf').style.display = 'block';
+    document.querySelector('.popup-cerf img').src = sgsi.getAttribute('src');
+});
+
+exitBtn.addEventListener('click', function(){
+    document.querySelector('.popup-cerf').style.display = 'none';
 })
+
